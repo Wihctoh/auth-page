5b53321f91e6502dd9e98733579ea3e718f99a5d
wihctoh <rivertoxi911@gmail.com>
2023-03-08 22:42:38 +0300

    complite task

--- js/singUp.js
+++ js/singUp.js
@@ -1,13 +1,14 @@
-function isValid(inpLogin_, inpPass_) {
-  if (!inpLogin_.value && !inpPass_.value) {
+function isValid(inpLogin_, inpPass_, inpPassConfirm_) {
+  if (!inpLogin_.value && !inpPass_.value && !inpPassConfirm_.value) {
     inpLogin_.style = "border: 1px solid red";
     inpPass_.style = "border: 1px solid red";
+    inpPassConfirm_.style = "border: 1px solid red";
     throw new Error("enter your login and password");
   }
 
   if (
     !/^\+[0-9]{3,3}[0-9]{2,2}[0-9]{7,7}$/gm.test(inpLogin_.value) &&
-    !/^\w+\@[a-zA-Z]+\.[a-z]{2,4}$/gm.test(inpLogin_.value)
+    !/^\w+\@[a-zA-Z]+\.[a-z]{2,4}$/gm.test(inpLogin_.value) 
   ) {
     inpLogin_.style = "border: 1px solid red";
     inpLogin_.value = "";
@@ -19,16 +20,25 @@ function isValid(inpLogin_, inpPass_) {
     inpPass_.value = "";
     throw new Error("password must be at least 8 characters");
   }
+
+  if (inpPass_.value != inpPassConfirm_.value) {
+    inpPass_.style = "border: 1px solid red";
+    inpPassConfirm_.style = "border: 1px solid red";
+    inpPass_.value = "";
+    inpPassConfirm_.value = "";
+    throw new Error("Password mismatch!");
+  }
 }
 
 document.querySelector(".btn-logIn").addEventListener("click", function () {
   try {
     const inpLogin = document.querySelector(".login");
     const inpPass = document.querySelector(".pass");
+    const inpPassConfirm = document.querySelector(".confirm-password");
 
-    isValid(inpLogin, inpPass);
+    isValid(inpLogin, inpPass, inpPassConfirm);
 
-    alert("You are successfully authorized in the system");
+    alert("You are successfully registered in the system");
   } catch (error) {
     alert(error.message);
   }
@@ -41,3 +51,10 @@ document.querySelector(".login").addEventListener("click", function () {
 document.querySelector(".pass").addEventListener("click", function () {
   document.querySelector(".pass").style = "border: 1px solid #fff";
 });
+
+document
+  .querySelector(".confirm-password")
+  .addEventListener("click", function () {
+    document.querySelector(".confirm-password").style =
+      "border: 1px solid #fff";
+  });
